a3036824e7d527402daba5cbbe29a73374fec489
Anna <anna.zareba@wp.eu>
2021-11-19 21:22:51 +0100

    Nauka podstawowych funkcji.

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main (String args[]) {
+
+        int age = 25;
+        String firstname = "Anna";
+
+        System.out.println("Witaj Swiecie.");
+        System.out.println( firstname + " ma " + age + " lat." );
+        System.out.println(age + " to piękna liczba.");
+        System.out.println("Za rok będziesz mieć " + (age + 1) + " lat.");
+
+    }
+}
